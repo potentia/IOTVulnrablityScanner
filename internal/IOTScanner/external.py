e43bbcccadddf05102d1b30eab6240d8ec1dd705
Harvey Passingham <passingh@uni.coventry.ac.uk>
2022-04-10 15:08:01 +0100

    Getting device data onto the website

--- internal/IOTScanner/external.py
+++ internal/IOTScanner/external.py
@@ -1,4 +1,15 @@
 
+from asyncio.windows_events import NULL
+from logging import NullHandler
+
+
 def testing():
-    vulrnableDevices = ["Device1", "Device2"]
+    vulrnableDevices = [["Device Name", "Finger Print"] ,["Device Name","Finger Print"]]
+    nullList =  [["Device Name", "Finger Print"] ,["Device Name","Finger Print"]]
+    if len(nullList)==0:
+        print("empty")
+    else:
+        print("Not empty")
     return vulrnableDevices
+
+testing()
