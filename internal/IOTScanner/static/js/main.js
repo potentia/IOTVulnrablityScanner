833e9e7c3519558ad97e1db912aa5b4094d49e11
potentia <harveypassingham@outlook.com>
2022-04-15 10:10:52 +0100

    bug fix

--- internal/IOTScanner/static/js/main.js
+++ internal/IOTScanner/static/js/main.js
@@ -3,7 +3,7 @@ $(function () {
     $("#scan").on('click', function(){
         $("#loading").show();
         $("#scan").hide();
-        $("#maintext").html("Scanning Your Network For Vulnrable IOT Devices")
+        $("#maintext").html("Scanning Your Network For Vulnerable IOT Devices")
     })
 
 });
